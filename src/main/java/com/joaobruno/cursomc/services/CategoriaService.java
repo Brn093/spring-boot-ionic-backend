22dd3fd9e8a28bc234a0843c6a256a18d037ccbc
João Bruno <brunofreitas.93@hotmail.com>
2020-06-28 03:53:01 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/joaobruno/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.joaobruno.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaobruno.cursomc.domain.Categoria;
+import com.joaobruno.cursomc.repositories.CategoriaRepository;
+import com.joaobruno.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+}
