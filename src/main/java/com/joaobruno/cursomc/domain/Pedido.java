a60a9e3aa4110f71d40b9d846f2522658d4f01a2
João Bruno <brunofreitas.93@hotmail.com>
2020-07-25 20:55:55 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/joaobruno/cursomc/domain/Pedido.java
+++ src/main/java/com/joaobruno/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.joaobruno.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -33,6 +36,9 @@ public class Pedido implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+	
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {}
 
@@ -91,6 +97,14 @@ public class Pedido implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -108,6 +122,5 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
-	
 	
 }
--- src/main/java/com/joaobruno/cursomc/domain/Produto.java
+++ src/main/java/com/joaobruno/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.joaobruno.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -31,11 +34,11 @@ public class Produto implements Serializable {
 		joinColumns = @JoinColumn(name = "produto_id"),
 		inverseJoinColumns = @JoinColumn(name="categoria_id")
 	)
+	
 	private List<Categoria> categorias = new ArrayList<>();
 	
-	public Integer getId() {
-		return id;
-	}
+	@OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	@Override
 	public int hashCode() {
@@ -61,7 +64,10 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
-
+	
+	public Integer getId() {
+		return id;
+	}
 	public void setId(Integer id) {
 		this.id = id;
 	}
@@ -89,6 +95,13 @@ public class Produto implements Serializable {
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+	
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	public Produto() {		
 	}
@@ -98,7 +111,16 @@ public class Produto implements Serializable {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
-	}
+	}	
 	
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		
+		for(ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		
+		return lista;		
+	}
 	
 }
